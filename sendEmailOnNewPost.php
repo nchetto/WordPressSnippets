6a691a2c083b8e3c1c8be7c9777ca28692f9b8f8
nchetto <57009955+nchetto@users.noreply.github.com>
2024-05-03 11:52:12 +0200

    Update sendEmailOnNewPost.php

--- sendEmailOnNewPost.php
+++ sendEmailOnNewPost.php
@@ -3,26 +3,29 @@
 // Send email to a list of recipients when a new post is published
 function send_email_on_new_post($post_id, $post, $old_status) {
 
-	// Recipient list
-	$to_list = array('example@email.com','example@email.com');
-	
+    // Recipient list
+    $to_list = array(
+        'example@email.com',
+        'example@email.com'
+    );
+
     // Ensure the function runs only once per post when post is published
     // Modify the post status to send also when post is updated or trashed
-	if ($old_status != 'publish'){
-	
-		// Prepare email data
-		$post_url = get_permalink($post_id);
-		$post_title = get_the_title($post_id);
-		$subject = "New post on my site - {$post_title}";
-		$message = "Hi,\n\nA new post has just been published: {$post_title}\nYou can find it at: {$post_url}\n\nThanks!";
-		$headers = 'From: Notifier <noreply@email.com>'; 
+    if ($old_status != 'publish') {
 
-		// Send email to each recipient
-		foreach ($to_list as $to) {
+        // Prepare email data
+        $post_url = get_permalink($post_id);
+        $post_title = get_the_title($post_id);
+        $subject = "New post on my site - {$post_title}";
+        $message = "Hi,\n\nA new post has just been published: {$post_title}\nYou can find it at: {$post_url}\n\nThanks!";
+        $headers = 'From: Notifier <noreply@email.com>';
+
+        // Send email to each recipient
+        foreach ($to_list as $to) {
             // Use wp_mail() to send email
-			wp_mail($to, $subject, $message, $headers);
-		}
-	}
+            wp_mail($to, $subject, $message, $headers);
+        }
+    }
 }
 
 // Hook into the publish_post action
