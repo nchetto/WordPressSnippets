bc6f33e13539c570b9ff9757d84f9f9c2190a906
nchetto <57009955+nchetto@users.noreply.github.com>
2024-05-08 12:24:36 +0200

    Fixed tab error

--- sendEmailOnNewEvent.php
+++ sendEmailOnNewEvent.php
@@ -4,14 +4,14 @@
 // Events are posts created by https://theeventscalendar.com/ as tribe_events
 function send_email_on_new_event( $new_status, $old_status, $post ) {
 
-    // Recipient list
-    $to_list = array(
-        'example@email.com',
-        'example@email.com'
-    );
+	// Recipient list
+	$to_list = array(
+		'example@email.com',
+		'example@email.com'
+	);
 	
-    // Ensure the function runs only once per post when post is published
-    // Modify the post status to send also when post is updated or trashed
+	// Ensure the function runs only once per post when post is published
+	// Modify the post status to send also when post is updated or trashed
 	if ( $old_status == $new_status || $new_status != 'publish' )
 		return;
 	
@@ -20,12 +20,12 @@ function send_email_on_new_event( $new_status, $old_status, $post ) {
 	if ( $post_type != 'tribe_events')
 		return;
 
-    // Prepare email data
+	// Prepare email data
 	$event_url = tribe_get_event_link($post);
 	$event_title = $post->post_title;
 	$event_title = html_entity_decode($event_title, ENT_QUOTES, 'UTF-8');
 	
-    if($event_url != ''){
+	if($event_url != ''){
 		$subject = "{$event_title} - New event on my site";
 		$message = "Hi,\n\nA new post has just been published: {$event_title}\nYou can find it at: {$event_url}\n\nThanks!";
 		$headers = 'From: Notifier <noreply@email.com>'; 
--- sendEmailOnNewPost.php
+++ sendEmailOnNewPost.php
@@ -3,30 +3,30 @@
 // Send email to a list of recipients when a new post is published
 function send_email_on_new_post($post_id, $post, $old_status) {
 
-    // Recipient list
-    $to_list = array(
-        'example@email.com',
-        'example@email.com'
-    );
+	// Recipient list
+	$to_list = array(
+		'example@email.com',
+		'example@email.com'
+	);
 
-    // Ensure the function runs only once per post when post is published
-    // Modify the post status to send also when post is updated or trashed
-    if ($old_status != 'publish') {
+	// Ensure the function runs only once per post when post is published
+	// Modify the post status to send also when post is updated or trashed
+	if ($old_status != 'publish') {
 
-        // Prepare email data
-        $post_url = get_permalink($post_id);
-        $post_title = get_the_title($post_id);
-        $post_title = html_entity_decode($post_title, ENT_QUOTES, 'UTF-8');
-        $subject = "New post on my site - {$post_title}";
-        $message = "Hi,\n\nA new post has just been published: {$post_title}\nYou can find it at: {$post_url}\n\nThanks!";
-        $headers = 'From: Notifier <noreply@email.com>';
+		// Prepare email data
+		$post_url = get_permalink($post_id);
+		$post_title = get_the_title($post_id);
+		$post_title = html_entity_decode($post_title, ENT_QUOTES, 'UTF-8');
+		$subject = "New post on my site - {$post_title}";
+		$message = "Hi,\n\nA new post has just been published: {$post_title}\nYou can find it at: {$post_url}\n\nThanks!";
+		$headers = 'From: Notifier <noreply@email.com>';
 
-        // Send email to each recipient
-        foreach ($to_list as $to) {
-            // Use wp_mail() to send email
-            wp_mail($to, $subject, $message, $headers);
-        }
-    }
+		// Send email to each recipient
+		foreach ($to_list as $to) {
+			// Use wp_mail() to send email
+			wp_mail($to, $subject, $message, $headers);
+		}
+	}
 }
 
 // Hook into the publish_post action
